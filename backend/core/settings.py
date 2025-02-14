265bb079c46680c6c492ae4e1c6d31acf5f14ce2
GaubergEONE <127289500+GaubergEONE@users.noreply.github.com>
2025-02-14 07:11:03 +0300

    !

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -163,3 +163,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost:8080/").split(",")
